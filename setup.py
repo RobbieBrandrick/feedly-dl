71a48bf3b19da9f0106bfbf8406da24b4caffed7
Robbie Brandrick <6001574+RobbieBrandrick@users.noreply.github.com>
2020-12-16 17:13:03 -0500

    Updated dependencies

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ setup(
     name='feedly-dl',
     version='0.1', 
     description="Download unread videos by category on feedly",
+    author="Robbie Brandrick",
+    url="",
     packages = find_packages(),
+    install_requires = [
+        'feedly-client',
+        'youtube_dl'
+    ],
     entry_points = {
         'console_scripts': ['feedly-dl=feedlydl.command_line:main'],
-    },
-    dependency_links=[
-        'https://github.com/ytdl-org/youtube-dl',
-        'https://github.com/feedly/python-api-client'
-    ]
+    },    
 )
